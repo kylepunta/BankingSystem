fc53932f9f9a8acb575b0ff52de058c5e959808c
GreatBrandon <brandon2005j@gmail.com>
2025-01-20 15:11:38 +0000

    Test menus

---
+++ test menu/script.js
@@ -0,0 +1,19 @@
+const dropdownChildren = document.querySelectorAll(".dropdown > *:not(h3)");
+const dropdowns = document.querySelectorAll(".dropdown");
+
+dropdownChildren.forEach((dropdownChildren) => {
+    dropdownChildren.classList.toggle("hidden");
+})
+
+dropdowns.forEach((dropdown) => {
+    dropdown.addEventListener("click", (event) => {
+        // Prevent event from bubbling up and affecting parent dropdowns
+        event.stopPropagation();
+
+        // Toggle the 'hidden' class for all child elements of the clicked dropdown
+        const children = dropdown.querySelectorAll(":scope > *:not(h3)"); // Select all child elements except <h3>
+        children.forEach((child) => {
+            child.classList.toggle("hidden");
+        });
+    });
+});
