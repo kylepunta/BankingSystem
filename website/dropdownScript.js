4d64412f43658aaf0462f14a6651bfad300b1882
GreatBrandon <brandon2005j@gmail.com>
2025-01-27 23:18:56 +0000

    fixed sidenav animations

--- website/dropdownScript.js
+++ website/dropdownScript.js
@@ -1,20 +1,59 @@
-const dropdownChildren = document.querySelectorAll(".dropdown > *:not(button)");
+const uls = document.querySelectorAll(".dropdown > ul");
 const dropdowns = document.querySelectorAll(".dropdown");
 
-dropdownChildren.forEach((dropdownChildren) => {
-    dropdownChildren.classList.toggle("hidden");
-})
+uls.forEach((ul) => {
+    ul.style.maxHeight = '0px';
+    ul.classList.toggle("hidden");
+});
+let leveltwos = 1;
+const doubleSize = 182.4;
 
 dropdowns.forEach((dropdown) => {
+    let expanded = false;
     dropdown.addEventListener("click", (event) => {
         // Prevent event from bubbling up and affecting parent dropdowns
         event.stopPropagation();
 
-        // Toggle the 'hidden' class for all child elements of the clicked dropdown
-        const children = dropdown.querySelectorAll(":scope > *:not(button)"); // Select all child elements except <button>
-        children.forEach((child) => {
-            child.classList.toggle("hidden");
+        const li = dropdown.querySelectorAll("li");
+
+        let totalHeight = 0;
+        li.forEach((l) => {
+            totalHeight = totalHeight + parseFloat(getComputedStyle(l).paddingTop) + (parseFloat(getComputedStyle(l).fontSize)*1.6);
         });
+
+        const mgm = dropdown.querySelector(".management");
+        if (mgm != null) {
+            totalHeight = totalHeight*1.6;
+        }
+
+        const dd = dropdown.querySelector(".double");
+        if (dd != null) {
+            totalHeight = totalHeight/3*leveltwos;
+        }
+
+        const l2 = dropdown.querySelector(".leveltwo");
+        if (l2 != null) {
+            const parent = l2.parentElement.parentElement;
+            if (!expanded) {
+                leveltwos++;
+            } else {
+                leveltwos--;
+            }
+            parent.style.maxHeight = doubleSize*leveltwos+'px';
+        }
+
+        const ul = dropdown.querySelector("ul");
+        if (!expanded) {
+            ul.style.maxHeight = totalHeight+'px';
+            ul.addEventListener("transitionend", () => {
+                ul.classList.toggle("hidden");
+            }, {once: true});
+        } else {
+            ul.style.maxHeight = '0px';
+            ul.classList.toggle("hidden");
+        }
+        expanded = !expanded;
+
         const button = dropdown.querySelector(".material-symbols-outlined");
         button.classList.toggle("arrow");
     });
