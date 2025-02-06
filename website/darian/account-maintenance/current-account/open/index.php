e2af03757d63183deedd2b34b6b6d9c3092f16e8
DarianByrne <173167730+DarianByrne@users.noreply.github.com>
2025-02-06 15:46:01 +0000

    Added Darian screens

---
+++ website/darian/account-maintenance/current-account/open/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="/commonStyles.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=keyboard_arrow_down" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="./open.css">
+    <script src="./open.js"></script>
+</head>
+
+<body>
+    <?php require($_SERVER["DOCUMENT_ROOT"] . '/sideMenu.html'); ?>
+    <main>
+        <h1>Open Current Account</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad quia obcaecati dolore nulla distinctio rem dolores fugit quisquam in temporibus, est expedita id, assumenda harum provident corporis pariatur, unde nobis.</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat reiciendis quis, ipsa earum ex veniam nulla unde aliquid labore dignissimos eveniet itaque molestias, numquam voluptates consectetur? Quasi modi officiis at!</p>
+        <form>
+            <label>Test name</label>
+            <input type="text" />
+        </form>
+    </main>
+</body>
+
+</html>
