1624c5f974e9a173c67e2b3cd79877645023c003
GreatBrandon <brandon2005j@gmail.com>
2025-02-27 19:17:32 +0000

    Open deposit account completed + started close deposit account

---
+++ website/brandon/open-deposit-account/processForm.php
@@ -0,0 +1,58 @@
+<?php 
+	session_start();
+    require '../../db.inc.php';
+    date_default_timezone_set("UTC");
+	if (isset($_POST['checkDetails'])) {
+		$sql = "SELECT * FROM Customer WHERE customerNo='".$_POST['number'] . "' AND deletedFlag='0'";
+
+		if (!$result = mysqli_query($con, $sql)) {
+			die('Error in querying the database ' . mysqli_error($con));
+		}
+
+		$rowcount = mysqli_affected_rows($con);
+
+		$_SESSION['number'] = $_POST['number'];
+
+		if ($rowcount == 1) {
+			$row = mysqli_fetch_array($result);		
+			$_SESSION['name'] = $row['firstName'] . " " . $row['surName'];
+			$_SESSION['address'] = $row['address'];
+			$_SESSION['eircode'] = $row['eircode'];
+			$_SESSION['dob'] = $row['dateOfBirth'];
+		} else if ($rowcount == 0) {
+			unset ($_SESSION['name']);
+			unset ($_SESSION['address']);
+			unset ($_SESSION['eircode']);
+			unset ($_SESSION['dob']);
+		} 
+
+		mysqli_close($con);
+		header('Location: ./');
+	} else if (isset($_POST['confirmDetails'])) {
+		$valid = false;
+		while (!$valid) {
+			// generate a random 8 digit account number
+			$accountNo = rand(10000000, 99999999);
+
+			$sql = "SELECT accountNumber FROM `Deposit Account` WHERE accountNumber=" . $accountNo . " UNION SELECT accountNumber FROM `Loan Account` WHERE accountNumber=" . $accountNo . " UNION SELECT accountNumber FROM `Current Account` WHERE accountNumber=" . $accountNo;
+
+			if (!$result = mysqli_query($con, $sql)) {
+				die('Error in querying the database ' . mysqli_error($con));
+			}
+
+			$rowCount = mysqli_affected_rows($con);
+
+			if ($rowCount == 0) {
+				$valid = true;
+				$_SESSION['accNo'] = $accountNo;
+				$_SESSION['number'] = $_POST['number'];
+				$_SESSION['name'] = $_POST['name'];
+				$_SESSION['address'] = $_POST['address'];
+				$_SESSION['eircode'] = $_POST['eircode'];
+				$_SESSION['dob'] = $_POST['dob'];
+			}
+		}
+		mysqli_close($con);
+		header('Location: ./');
+	}
+?>
--- website/brandon/open-deposit-account/script.js
+++ website/brandon/open-deposit-account/script.js
@@ -1,9 +1,8 @@
 function populate() {
     // get the value from the dropdown menu and put them into the fields
-    var sel = document.getElementById("listbox");
-    var result;
-    result = sel.options[sel.selectedIndex].value;
-    var personDetails = result.split(' ¬'); // ¬ has to be used as addresses may contain a , inside them breaking the string
+    const sel = document.getElementById("listbox");
+    const result = sel.options[sel.selectedIndex].value;
+    const personDetails = result.split(' ¬'); // ¬ has to be used as addresses may contain a , inside them breaking the string
 	document.getElementById("number").value = personDetails[0];
     document.getElementById("name").value = personDetails[1];
     document.getElementById("address").value = personDetails[2];
@@ -12,16 +11,20 @@ function populate() {
 }
 
 function confirmSubmit() {
-    const name = document.getElementById("name").value;
-    const address = document.getElementById("address").value;
-    const eircode = document.getElementById("eircode").value;
-    const dob = document.getElementById("dob").value;
+	const actionButton = document.activeElement; // Get the clicked button (submit)
+    const action = actionButton.value; // Get the value of the clicked button
+	
+	if (document.activeElement.value === "Confirm details") {
+		const name = document.getElementById("name").value;
+		const address = document.getElementById("address").value;
+		const eircode = document.getElementById("eircode").value;
+		const dob = document.getElementById("dob").value;
 
-    if (!name || !address || !eircode || !dob) {
-        return false; 
-    } else {
-        return true;
-    }
+		if (!name || !address || !eircode || !dob) {
+			return false; 
+		}
+	}
+	return true;
 }
 
 function toggleInputs() {
@@ -31,9 +34,8 @@ function toggleInputs() {
 		document.getElementById("listbox").disabled = true;
 		document.getElementById("chooseCustomerButton").disabled = true;
 		document.getElementById("confirmCustomerButton").disabled = true;
-		document.getElementById("number").disabled = true;
+		document.getElementById("number").readOnly = true;
 		document.getElementById("submitCustomer").disabled = false;
-		populate();
 	}
 }
 
